e2238e207fced7798fcb39c0e654c501f0dba1f3
mfturkcan <mfturkcan@gmail.com>
2023-01-06 13:29:29 +0300

    add concurrency

---
+++ concurrency/concurrency.go
@@ -0,0 +1,38 @@
+package main
+
+type WebsiteChecker func(string) bool
+type result struct {
+	url     string
+	success bool
+}
+
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+	resultChannel := make(chan result)
+
+	for _, url := range urls {
+		go func(u string) {
+			resultChannel <- result{u, wc(u)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChannel
+		results[r.url] = r.success
+	}
+
+	return results
+}
+
+func CheckWebsitesNotConcurrent(wc WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+
+	for _, url := range urls {
+		results[url] = wc(url)
+	}
+	return results
+}
+
+func main() {
+
+}
---
+++ concurrency/concurrency_test.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func mockWebsiteChecker(url string) bool {
+	return url != "waat://furhurterwe.geds"
+}
+
+func TestCheckWebsites(t *testing.T) {
+	websites := []string{
+		"http://google.com",
+		"http://blog.gypsydave5.com",
+		"waat://furhurterwe.geds",
+	}
+	wc := mockWebsiteChecker
+
+	want := map[string]bool{
+		"http://google.com":          true,
+		"http://blog.gypsydave5.com": true,
+		"waat://furhurterwe.geds":    false,
+	}
+
+	got := CheckWebsites(wc, websites)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Expected %v but got %v", want, got)
+	}
+}
+
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+
+// 32_204_013 ns/op
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecker, urls)
+	}
+}
+
+// 3_096_785_500 ns/op
+func BenchmarkCheckWebsitesNotConcurrent(b *testing.B) {
+	urls := make([]string, 100)
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		CheckWebsitesNotConcurrent(slowStubWebsiteChecker, urls)
+	}
+}
